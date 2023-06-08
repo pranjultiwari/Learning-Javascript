6dff60a97d521fe781f833bd3c79092082324619
Pranjul Tiwari <pranjultiwari7800@gmail.com>
2023-06-08 23:29:43 +0530

    commit

--- 14_callback_function.js
+++ 14_callback_function.js
@@ -1,5 +1,7 @@
 var result = [2,4,6,8].every((e)=> (e % 2 === 0));
 console.log(result);
+console.log(result);
+
 
 // syntax: 1 - without using return keyword.
 // var sample = [a,b,c,d].every(() => () );
